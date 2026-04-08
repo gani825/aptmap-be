e45a57da4fc85d431442616e22320f213e069352
gani825 <kgee0503@gmail.com>
2026-04-08 23:08:51 +0900

    chore: 프로젝트 초기 세팅 및 application.yaml 설정

---
+++ src/main/java/com/aptmapbe/AptmapBeApplication.java
@@ -0,0 +1,13 @@
+package com.aptmapbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AptmapBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AptmapBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aptmapbe/AptmapBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aptmapbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AptmapBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
